9e829dd0dc4805b2131be00e7797053b1c7a060d
root <root@manuel-ESPRIMO-P>
2014-03-25 21:31:05 +0100

    ok

---
+++ src/com/iesebre/DAM2/googlemaps/MainActivity.java
@@ -0,0 +1,27 @@
+package com.iesebre.DAM2.googlemaps;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends android.support.v4.app.FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
